3b73834b033c2bf5ed8e42922b7602f0b9db683a
Carro080323 <hedbergcarolina@gmail.com>
2025-08-27 13:56:43 +0200

    first

---
+++ modul.py
@@ -0,0 +1,24 @@
+print("Hello World")
+
+print('Han sa: "Det här är ett citat."')
+
+print("""Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum""")
+
+print("Första\nAndra\nTredje")
+
+print("Fy " + "17!")
+
+namn = input("Skriv in ditt namn: ")
+print("Du heter " + namn)
+
+user_namn = input("Vad heter du? ")
+print("Välkommen",user_namn)
